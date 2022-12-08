c37a530c0162e7af24127bdbb515242feaf87770
jennie Forsell <forselljennie@gmail.com>
2022-12-08 12:54:59 +0100

    ändrade cy.visit till roten av mitt projekt

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,54 +1,40 @@
 import { mockData } from "./../../src/ts/services/__mocks__/movieservice";
 
-describe("search movies", () => {
-  it("should show my webside", () => {
-    cy.visit("http://localhost:1234");
-  });
+beforeEach(() => {
+  cy.visit("/");
+});
 
+describe("search movies", () => {
   it("should put text in input", () => {
-    cy.visit("http://localhost:1234");
     cy.get("input").type("text").should("have.value", "text");
   });
 
   it("should have button", () => {
-    cy.visit("http://localhost:1234");
     cy.get("button").contains("Sök");
   });
 
-  it("should get 3 headings", () => {
-    cy.visit("http://localhost:1234");
-    cy.intercept("GET", "http://omdbapi.com/*", mockData);
-
-    cy.get("form").submit();
-    cy.get("div.movie > h3").should("have.length", 3);
-  });
-
   it("should get movies", () => {
-    cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
     cy.get("div#movie-container > div.movie").should("have.length", 3);
   });
 
-  it("should get fake last heading", () => {
-    cy.visit("http://localhost:1234");
-    cy.intercept("GET", "http://omdbapi.com/*", mockData);
-
-    cy.get("form").submit();
-    cy.get("div.movie:last > h3").contains("Pelle svanslös");
-  });
-
-  it("should get heading", () => {
-    cy.visit("http://localhost:1234");
+  it("should get headings", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
     cy.get("div.movie > h3").should("have.length", 3);
   });
 
-  it("should get fake img", () => {
-    cy.visit("http://localhost:1234");
+  it("should get last heading", () => {
+    cy.intercept("GET", "http://omdbapi.com/*", mockData);
+
+    cy.get("form").submit();
+    cy.get("div.movie:last > h3").contains("Pelle svanslös");
+  });
+
+  it("should get img", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -58,7 +44,6 @@ describe("search movies", () => {
 
 describe("displayNoResult", () => {
   it("should have error message", () => {
-    cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", { fakeSearch: [] });
 
     cy.get("form").submit();
@@ -66,7 +51,6 @@ describe("displayNoResult", () => {
     cy.get("div#movie-container > p").contains("Inga sökresultat att visa");
   });
   it("should have 0 movies", () => {
-    cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", { fakeSearch: [] });
 
     cy.get("form").submit();
